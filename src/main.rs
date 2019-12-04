459832a2b2b44a85a4c8e9c52e2cd5c633ee5559
David Sanders <davesque@gmail.com>
2019-12-04 12:25:57 -0700

    Add example minus type parameters

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,27 @@
 use nom::bytes::complete::is_not;
 use nom::character::complete::multispace0;
 use nom::combinator::verify;
-use nom::error::{
-    ParseError,
-    VerboseError,
-};
+use nom::error::VerboseError;
 use nom::sequence::terminated;
 use nom::IResult;
 
-fn one_token<'a, E>(input: &'a str) -> IResult<&str, &str, E>
-where
-    E: ParseError<&'a str>,
-{
+fn one_token(input: &str) -> IResult<&str, &str, VerboseError<&str>> {
     terminated(is_not(" \t\r\n"), multispace0)(input)
 }
 
-fn str_token<'a, E>(expected_string: String) -> impl Fn(&'a str) -> IResult<&str, &str, E>
-where
-    E: ParseError<&'a str>,
-{
+fn str_token<'a>(
+    expected_string: String,
+) -> impl Fn(&'a str) -> IResult<&str, &str, VerboseError<&str>> {
     verify(one_token, move |actual_string| {
         actual_string == expected_string
     })
 }
 
 fn main() {
-    let parser_1 = str_token::<VerboseError<_>>("foo".into());
+    let parser_1 = str_token("foo".into());
     let string = "foo bar".to_string();
     let input = &string[..];
-    let parser_2 = str_token::<VerboseError<_>>("foo".into());
+    let parser_2 = str_token("foo".into());
 
     println!("{:?} {:?}", parser_1(input), parser_2(input),);
 }
